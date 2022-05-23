1a99174009c18e20fab4b6c3e1a90906ba6988f1
Afaq Babar <afaqbabar@gmail.com>
2022-05-23 13:06:45 +0000

    infra.tf

--- infra.tf
+++ infra.tf
@@ -50,19 +50,4 @@ resource "ionoscloud_k8s_node_pool" "k8s_node_pool_02" {
   storage_size      = 40
   public_ips        = [ionoscloud_ipblock.ipblock_01.ips[0], ionoscloud_ipblock.ipblock_01.ips[1]]
 
-
-}
-
-
-
-provider "kubernetes" {
-  host  = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].cluster.server
-  token = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].users[0].user.token
-  #config_path = local_file.kubeconfig.filename
-  cluster_ca_certificate = data.ionoscloud_k8s_cluster.k8s_cluster_01.ca_crt
-}
-
-resource "local_file" "kubeconfig" {
-  sensitive_content = yamlencode(jsondecode(data.ionoscloud_k8s_cluster.k8s_cluster_01.kube_config))
-  filename          = "kubeconfig.yaml"
 }
