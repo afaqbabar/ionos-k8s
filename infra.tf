6a3cf48ca34c2d3de49e29f9c3c18bd5a7999ea5
Afaq Babar <afaqbabar@gmail.com>
2022-05-27 14:51:26 +0000

    create 2nd k8s cluster

--- infra.tf
+++ infra.tf
@@ -1,9 +1,9 @@
 resource "ionoscloud_k8s_cluster" "k8s_cluster_02" {
-  name                  = "k8sCluster_02"
-  k8s_version           = "1.23.6"
-  public                = true
+  name        = "k8sCluster_02"
+  k8s_version = "1.23.6"
+  public      = true
   maintenance_window {
-    day_of_the_week     = "Sunday"
-    time                = "09:00:00Z"
+    day_of_the_week = "Sunday"
+    time            = "09:00:00Z"
   }
 }
